bf9e02653898f2f60898580aa9f71f0d70509cb2
Federico Delfino <google-oauth2|115203993649183592529>
2022-06-15 15:50:28 +0000

    set up project

---
+++ definitions/01_temp/definitions.js
@@ -0,0 +1,20 @@
+declare({
+    name: "seo_ga_source_traffic",
+    schema: "seo_dashboard_01_temp",
+    description: ""
+})
+declare({
+    name: "seo_gsc_page_query",
+    schema: "seo_dashboard_01_temp",
+    description: ""
+})
+declare({
+    name: "seo_gsc_total",
+    schema: "seo_dashboard_01_temp",
+    description: ""
+})
+declare({
+    name: "seo_semrush_ranking",
+    schema: "seo_dashboard_01_temp",
+    description: ""
+})
